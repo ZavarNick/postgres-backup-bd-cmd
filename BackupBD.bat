14658b66077492bd4f18b9e009a4ef292c250636
ZavarNick <emelyanoffon@gmail.com>
2024-11-20 16:51:45 +0500

    Изменение коментов

--- BackupBD.bat
+++ BackupBD.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Общие параметры
+rem General parameters
 set PGPASSWORD=qwerty
 set PGUSER=postgres
 set PGHOST=localhost
@@ -11,16 +11,16 @@ set BACKUP_DIR=D:\backups_actual
 set DATE=%DATE:~0,2%%DATE:~3,2%%DATE:~-4%
 set LOG=%BACKUP_DIR%\log_%DATE%.txt
 
-rem Удаление старых бекапов (старше 7 дней)
+rem Deleting old backups (older than 7 days)
 forfiles /p "%BACKUP_DIR%" /m *.backup /D -7 /C "cmd /c del /q @path"
 
-rem Создание каталога для хранения бекапов, если его нет
+rem Creating a directory for storing backups, if there is none
 if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
 
-rem Список баз данных
+rem List of databases
 set DATABASES=name_base name_base2
 
-rem Бекап баз данных
+rem Database backup
 for %%D in (%DATABASES%) do (
     set "PGDATABASE=%%D"
     set "FILENAME=%BACKUP_DIR%\backup_%%D_%DATE%.backup"
@@ -32,5 +32,5 @@ for %%D in (%DATABASES%) do (
     )
 )
 
-rem Завершение
+rem Completion
 echo All backups completed. >> "!LOG!"
